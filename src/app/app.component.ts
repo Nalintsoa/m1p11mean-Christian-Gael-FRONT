e7b603a4fe521a2d398b5f97e209bb0530907484
nalytovo <nalytovo@gmail.com>
2024-02-07 09:19:16 +0300

    en attente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBars, faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'm1p11mean-christian-gael';
+
+  faBars = faBars;
+  faUserCircle = faUserCircle;
 }
