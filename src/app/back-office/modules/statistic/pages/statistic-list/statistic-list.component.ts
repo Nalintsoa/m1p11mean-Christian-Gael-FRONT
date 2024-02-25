fe151e7c8d142455f79b91f56fd61599184ec183
Sambatra Ga <sambatraga@gmail.com>
2024-02-25 19:34:14 +0300

    install chartjs

--- src/app/back-office/modules/statistic/pages/statistic-list/statistic-list.component.ts
+++ src/app/back-office/modules/statistic/pages/statistic-list/statistic-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-statistic-list',
@@ -8,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrl: './statistic-list.component.scss'
 })
 export class StatisticListComponent {
+  @ViewChild('CANVAS') elementRef?: ElementRef;
 
+  ngAfterViewInit() {
+    console.log(this.elementRef?.nativeElement)
+    const ctx = this.elementRef?.nativeElement
+    console.log(ctx)
+
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
+
+  test(e: any) {
+    console.log(e.target.value)
+  }
 }
