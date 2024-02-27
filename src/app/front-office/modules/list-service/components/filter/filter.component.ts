44ec4a2415e94bf4d91bf797815f589dd77e2681
Sambatra Ga <sambatraga@gmail.com>
2024-02-27 21:41:25 +0300

    filter service front office

--- src/app/front-office/modules/list-service/components/filter/filter.component.ts
+++ src/app/front-office/modules/list-service/components/filter/filter.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
@@ -5,17 +6,33 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-filter',
   standalone: true,
-  imports: [FaIconComponent],
+  imports: [FaIconComponent, CommonModule],
   templateUrl: './filter.component.html',
   styleUrl: './filter.component.scss'
 })
 export class FilterComponent {
   faSearch = faSearch;
-  @Output() keySearch = new EventEmitter<string>();
+  price?: number = 100000;
+  @Output() searchEmitter = new EventEmitter<string>();
+  @Output() categoryEmitter = new EventEmitter<string>();
+  @Output() prefTypeEmitter = new EventEmitter<string>();
+  @Output() priceEmitter = new EventEmitter<number>()
+
+  categoryItems = ['Tout', 'Manucure', 'Pédicure', 'Soin du visage', 'Massothérapie'];
 
   // TODO: filter services front-office
   onSearch(event: any) {
-    this.keySearch.emit(event.target.value)
+    this.searchEmitter.emit(event.target.value)
   }
+
+  onChangeCategory(event: any) {
+    this.categoryEmitter.emit(event.target.value);
+  }
+
+  onChangePrice(event: any) {
+    this.price = event.target.value;
+    this.priceEmitter.emit(event.target.value);
+  }
+
 
 }
