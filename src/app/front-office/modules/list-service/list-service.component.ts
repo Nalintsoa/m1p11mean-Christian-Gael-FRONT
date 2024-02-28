c87087dece98b6caf83491b8ce4d60237cdd0fc3
nalytovo <nalytovo@gmail.com>
2024-02-28 09:46:09 +0300

    fix

--- src/app/front-office/modules/list-service/list-service.component.ts
+++ src/app/front-office/modules/list-service/list-service.component.ts
@@ -20,7 +20,7 @@ export class ListServiceComponent {
   services: IService[] = [];
 
   searchQuery: string = '';
-  category: string = 'Tout';
+  category: string = '';
   defaultPrice = 1000000;
   price: number = this.defaultPrice;
 
